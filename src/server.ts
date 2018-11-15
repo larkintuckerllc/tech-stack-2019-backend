4e401a4853fa0f27c94ab1030f1be1924afebb79
John Tucker <jtucker@appirio.com>
2018-11-15 11:26:50 -0500

    First commit

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import cors from 'cors';
+import express from 'express';
+const app = express();
+
+app.use(cors());
+app.get('/', (req, res) => res.send({ hello: 'world' }));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
