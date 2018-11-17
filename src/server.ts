d36784310003c62fdbc5d850c935ad43dd5d556c
John Tucker <jtucker@appirio.com>
2018-11-17 07:56:57 -0500

    Refactor

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import env from './apis/env';
 
-const hello = env.helloValue;
+const hello = env.HELLO_VALUE;
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => res.send({ hello }));
