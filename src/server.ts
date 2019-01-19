e4bc2b20d242985d819b1d10bdaabe094e373a9e
John Tucker <jtucker@appirio.com>
2019-01-19 11:33:58 -0500

    Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import cors from 'cors';
 import express from 'express';
-const app = express();
 
+const app = express();
 app.use(cors());
 app.get('/', (req, res) => res.send({ hello: 'world' }));
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
