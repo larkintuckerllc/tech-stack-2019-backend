9e5bf1dbc3a96b45d180a3adb9c93cb756cb7bf7
John Tucker <jtucker@appirio.com>
2019-02-18 20:36:50 -0500

    Done

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
+import { ApolloServer, gql } from 'apollo-server-express';
 import cors from 'cors';
 import express from 'express';
 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => res.send({ hello: 'world' }));
+server.applyMiddleware({ app });
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
